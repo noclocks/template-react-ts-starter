efbf979bcee285e3ccd9ab529010e06e5b19719d
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2024-03-15 19:21:21 -0400

    config: add eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'prettier'
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+  },
+  ignorePatterns: [
+    'dist',
+    '.eslintrc.cjs'
+  ]
+}
