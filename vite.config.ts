dfa2caaa7100df5699368b3905e31530a733a916
Patrick Howard <patrick.howard@noclocks.dev>
2024-03-22 12:09:57 -0400

    add `v8` as `provider` in `vite` config

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     setupFiles: ['./tests/setupTests.ts'],
     watch: false,
     coverage: {
+      provider: 'v8',
       reporter: ['text', 'json-summary', 'json'],
       reportOnFailure: true,
     },
