dd7eebe5602942f161588c8cf242e8223e43ca8d
Patrick Howard <patrick.howard@noclocks.dev>
2024-03-22 12:33:07 -0400

    change `npm` to `pnpm` & `npx` to `pnpx`

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,9 +1,18 @@
 module.exports = {
-    trailingComma: "es5",
-    tabWidth: 2,
-    semi: true,
-    singleQuote: true,
-    printWidth: 120,
-    bracketSpacing: true,
-    endOfLine: "lf",
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 120,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  overrides: [
+    {
+      files: '*.md',
+      options: {
+        // Disable formatting for .md files
+        embeddedLanguageFormatting: 'off',
+      },
+    },
+  ],
 };
